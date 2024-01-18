7443ce9b2afd0af6169027d6605b54a189674fd5
Itbrandon <brandoniticka@gmail.com>
2024-01-17 20:12:29 -0800

    version-2

--- js/rps.js
+++ js/rps.js
@@ -2,13 +2,21 @@ let playGame = confirm("Let's Play Rock Paper Scissors");
 
 if(playGame)
 {
-  let move = prompt("Type 'rock', 'paper', or 'scissors'");
-  let playerMove = move.trim().toLowerCase();
-  let computerNumber = Math.floor((Math.random()*3)+1);
-  let computerMove = (computerNumber === 1) ? "rock" : (computerNumber === 2) ? "paper" :  "scissors";
+  while (playGame) {
+  const move = prompt("Type 'rock', 'paper', or 'scissors'");
 
-  let result = (playerMove === null) ? "Wait you didn't Pick an Action" : (playerMove !== 'rock' && playerMove !== 'paper' && playerMove !== 'scissors') ? "Not a proper action"
-  : (playerMove === computerMove) ? `Player: ${playerMove}\n\nComputer: ${computerMove}\n\nWe Tied`
+  if (move != null)
+  {
+  const playerMove = move.trim().toLowerCase();
+  
+
+
+    if(playerMove === 'rock' || playerMove === 'paper' || playerMove === 'scissors')
+    {
+  const computerNumber = Math.floor(Math.random() * 3);
+  const computerArray = ['rock', 'paper', 'scissors'];
+  const computerMove = computerArray[computerNumber];
+  const result = (playerMove === computerMove) ? `Player: ${playerMove}\n\nComputer: ${computerMove}\n\nWe Tied`
     : (playerMove === 'rock' && computerMove === 'scissors') ? `Player: ${playerMove}\n\nComputer: ${computerMove}\n\nYou Win`
       : (playerMove === 'rock' && computerMove === 'paper') ? `Player: ${playerMove}\n\nComputer: ${computerMove}\n\nYou Lose`
         : (playerMove === 'paper' && computerMove === 'rock') ? `Player: ${playerMove}\n\nComputer: ${computerMove}\n\nYou Win`
@@ -17,10 +25,23 @@ if(playGame)
               : `Player: ${playerMove}\n\nComputer: ${computerMove}\n\nYou Lose`;
                 
   alert(result);
-  let playAgain = confirm("Play Again?");
-  playAgain ? location.reload() : alert("Thanks for Playing");
-}
+  playGame = confirm("Play Again?");
+  if(!playGame) alert("Ok, thanks for playing");
+    }
 
+  else {
+    alert("Wait you didn't Pick a Proper Action");
+    continue;
+  }
+  }
+
+  else {
+    alert("I guess you changed your mind");
+    break;
+  }
+
+}
+}
 else {
   alert("ok, maybe next time")
 }
